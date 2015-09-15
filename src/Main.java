94fb424d8205fce3e813d3fbc7e1c8abd7ad75d0
GuusHamm <Dihw8vGH>
2015-09-15 13:00:03 +0200

    Setting up properties

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
-public class Main {
-    public static void main (String[] args) {
-        System.out.println(System.getenv("VAR"));
+import com.oracle.webservices.internal.api.message.PropertySet;
+import javafx.beans.property.SetProperty;
 
+import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+                System.out.println(System.getenv("VAR"));
+        // create and load default properties
+        Properties defaultProps = new Properties();
+        FileInputStream in = new FileInputStream("defaultProperties");
+        defaultProps.load(in);
+        in.close();
+
+// create application properties with default
+        Properties applicationProps = new Properties(defaultProps);
+
+
+
+        
     }
 }
