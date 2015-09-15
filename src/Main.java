81eba0b4f99b92f218ca63b1a4676410ffdc30e8
GuusHamm <Dihw8vGH>
2015-09-15 13:13:21 +0200

    Cleaned imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import com.oracle.webservices.internal.api.message.PropertySet;
-import javafx.beans.property.SetProperty;
-
 import java.io.IOException;
-import java.io.*;
-import java.util.Properties;
 
 public class Main {
     public static void main (String[] args) throws IOException {
