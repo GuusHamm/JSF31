c48e4b603b15320bb5ec21e7128e65f86a31f9bc
GuusHamm <Dihw8vGH>
2015-09-15 13:07:34 +0200

    innocent commit #1

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,6 @@ import java.util.Properties;
 public class Main {
     public static void main (String[] args) throws IOException {
                 System.out.println(System.getenv("VAR"));
-        // create and load default properties
-        Properties defaultProps = new Properties();
-        FileInputStream in = new FileInputStream("defaultProperties");
-        defaultProps.load(in);
-        in.close();
-
-// create application properties with default
-        Properties applicationProps = new Properties(defaultProps);
-
 
 
         
