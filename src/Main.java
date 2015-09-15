2994f95ca1e23514b2ab7b4d88244775a4385d95
GuusHamm <Dihw8vGH>
2015-09-15 14:50:25 +0200

    Fixed 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 public class Main {
     public static void main (String[] args) throws IOException {
                 System.out.println(System.getenv("VAR"));
 
         //http://crunchify.com/java-properties-file-how-to-read-config-properties-values-in-java/
-        
+
+        Properties props = new Properties();
+
+        props.setProperty("VAR", System.getenv("VAR"));
+        OutputStream os ;
+        File f = new File("server.properties");
+        OutputStream out = new FileOutputStream( f );
+        props.store(out,"Variable VAR");
+
     }
 }
