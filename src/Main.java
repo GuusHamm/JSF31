5e386b0b0b4cf1bee8e2d866f897cbb19daec13c
GuusHamm <Dihw8vGH>
2015-09-15 13:15:04 +0200

    Added a refernce

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String[] args) throws IOException {
                 System.out.println(System.getenv("VAR"));
 
-
+        //http://crunchify.com/java-properties-file-how-to-read-config-properties-values-in-java/
         
     }
 }
